6271454749e51c6bbbce21f99ef7a9120b674dc5
Philipp M. Scholl <pscholl@ese.uni-freiburg.de>
2015-02-25 09:36:52 +0100

    add FrequencyDomain extractor

---
+++ GRT/FeatureExtractionModules/FFT/FrequencyDomainFeatures.cpp
@@ -0,0 +1,117 @@
+/*
+ GRT MIT License
+ Copyright (c) <2012> <Nicholas Gillian, Media Lab, MIT>
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+ and associated documentation files (the "Software"), to deal in the Software without restriction, 
+ including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+ subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all copies or substantial 
+ portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
+ LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+ IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "FrequencyDomainFeatures.h"
+
+namespace GRT{
+    
+//Register the FrequencyDomainFeatures module with the FeatureExtraction base class
+RegisterFeatureExtractionModule< FrequencyDomainFeatures > FrequencyDomainFeatures::registerModule("FrequencyDomainFeatures");
+
+FrequencyDomainFeatures::FrequencyDomainFeatures(UINT fftWindowSize, UINT hopSize, UINT numDimensions,
+                            UINT fftWindowFunction, bool computeMaxFreqFeature, bool computeMaxFreqSpectrumRatio,
+                            bool computeCentroidFeature, bool computeTopNFreqFeatures, UINT N) {
+    classType = "FrequencyDomainFeatures";
+    featureExtractionType = classType;
+
+    infoLog.setProceedingText("[FrequencyDomainFeatures]");
+    warningLog.setProceedingText("[WARNING FrequencyDomainFeatures]");
+    errorLog.setProceedingText("[ERROR FrequencyDomainFeatures]");
+
+    fft = FFT(fftWindowSize, hopSize, numDimensions, fftWindowFunction, true, false);
+    fftf = FFTFeatures(fftWindowSize/2, numDimensions, computeMaxFreqFeature, computeMaxFreqSpectrumRatio,
+                       computeCentroidFeature, computeTopNFreqFeatures, N);
+}
+
+FrequencyDomainFeatures::FrequencyDomainFeatures(const FrequencyDomainFeatures &rhs){
+    infoLog.setProceedingText("[FrequencyDomainFeatures]");
+    warningLog.setProceedingText("[WARNING FrequencyDomainFeatures]");
+    errorLog.setProceedingText("[ERROR FrequencyDomainFeatures]");
+
+    //Invoke the equals operator to copy the data from the rhs instance to this instance
+    *this = rhs;
+}
+    
+FrequencyDomainFeatures::~FrequencyDomainFeatures(void){
+}
+    
+FrequencyDomainFeatures& FrequencyDomainFeatures::operator=(const FrequencyDomainFeatures &rhs){
+
+    if( this != &rhs ){
+        this->fft = rhs.fft;
+        this->fftf = rhs.fftf;
+    }
+    return *this;
+}
+
+//Clone method
+bool FrequencyDomainFeatures::deepCopyFrom(const FeatureExtraction *featureExtraction){ 
+    if( featureExtraction == NULL ) return false;
+
+    if( this->getFeatureExtractionType() == featureExtraction->getFeatureExtractionType() ){
+        //Invoke the equals operator to copy the data from the rhs instance to this instance
+        *this = *(FrequencyDomainFeatures*)featureExtraction;
+        return true;
+    }
+
+    errorLog << "clone(FeatureExtraction *featureExtraction) -  FeatureExtraction Types Do Not Match!" << endl;
+    return false;
+}
+
+bool FrequencyDomainFeatures::saveModelToFile(fstream &file) const{
+  return false;
+}
+
+bool FrequencyDomainFeatures::loadModelFromFile(fstream &file){
+  return false;
+}
+
+bool FrequencyDomainFeatures::computeFeatures(const VectorDouble &inputVector){
+  bool ok;
+  if (!fft.computeFeatures(inputVector)) {
+    errorLog << "computeFeatures(const VectorDouble &inputVector) - computing fft failed" << endl;
+    return false;
+  }
+
+  if (!fftf.computeFeatures(fft.getFeatureVector())) {
+    errorLog << "computeFeatures(const VectorDouble &inputVector) - computing fft failed" << endl;
+    return false;
+  }
+
+
+  featureDataReady = true;
+  featureVector = fftf.getFeatureVector();
+
+  return true;
+}
+
+bool FrequencyDomainFeatures::clear(){
+  //Clear the base class
+  FeatureExtraction::clear();
+  fft.clear();
+  fftf.clear();
+  return true;
+}
+
+bool FrequencyDomainFeatures::reset(){ 
+  return fftf.reset() && fft.reset();
+}
+
+}//End of namespace GRT
---
+++ GRT/FeatureExtractionModules/FFT/FrequencyDomainFeatures.h
@@ -0,0 +1,144 @@
+/**
+ GRT MIT License
+ Copyright (c) <2012> <Nicholas Gillian, Media Lab, MIT>
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or substantial
+ portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+ LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef GRT_FrequencyDomainFeatures_HEADER
+#define GRT_FrequencyDomainFeatures_HEADER
+
+#include "../../CoreModules/FeatureExtraction.h"
+#include "FastFourierTransform.h"
+#include "FFT.h"
+#include "FFTFeatures.h"
+
+namespace GRT{
+
+class FrequencyDomainFeatures : public FeatureExtraction
+{
+public:
+    /**
+     Constructor, sets the fftWindowSize, hopSize, fftWindowFunction, if the magnitude and phase should be computed during the FrequencyDomainFeatures and the number
+     of dimensions in the input signal.
+
+     @param UINT fftWindowSize: sets the size of the fft, this should be a power of two. Default fftWindowSize=512
+     @param UINT hopSize: sets how often the fft should be computed. If the hopSize parameter is set to 1 then the FrequencyDomainFeatures will be computed everytime
+     the classes computeFeatures(...) or computeFrequencyDomainFeatures(...) functions are called. You may not want to compute the FrequencyDomainFeatures of the input signal for every
+     sample however, if this is the case then set the hopSize parameter to N, in which case the FrequencyDomainFeatures will only be computed every N samples on the previous M values, where M is equal to the fftWindowSize. Default hopSize=1
+     @param UINT numDimensions: the dimensionality of the input data to the FrequencyDomainFeatures.  Default numDimensions = 1
+     @param UINT fftWindowFunction: sets the window function of the FrequencyDomainFeatures. This should be one of the FrequencyDomainFeaturesWindowFunctionOptions enumeration values. Default windowFunction=RECTANGULAR_WINDOW
+     @param bool computeMaxFreqFeature: sets if the maximum frequency feature will be included in the feature vector. Default value = true
+     @param bool computeMaxFreqSpectrumRatio: sets if the maximum-frequency spectrum-frequency ratio feature will be included in the feature vector. Default value = true
+     @param bool computeCentroidFeature: sets if the centroid frequency feature will be included in the feature vector. Default value = true
+     @param bool computeTopNFreqFeatures: sets if the top N frequency feature will be included in the feature vector. Default value = true
+     @param bool N: sets if size of N for the top N frequency features. Default value = 10
+     */
+    FrequencyDomainFeatures(UINT fftWindowSize=512,
+                            UINT hopSize=1,
+                            UINT numDimensions=1,
+                            UINT fftWindowFunction=FFT::RECTANGULAR_WINDOW,
+                            bool computeMaxFreqFeature = true,
+                            bool computeMaxFreqSpectrumRatio = true,
+                            bool computeCentroidFeature = true,
+                            bool computeTopNFreqFeatures = true,
+                            UINT N = 10);
+
+    /**
+     Copy Constructor, copies the FrequencyDomainFeatures from the rhs instance to this instance
+
+     @param const FrequencyDomainFeatures &rhs: another instance of the FrequencyDomainFeatures class from which the data will be copied to this instance
+     */
+    FrequencyDomainFeatures(const FrequencyDomainFeatures &rhs);
+
+    /**
+     Default Destructor
+     */
+    virtual ~FrequencyDomainFeatures(void);
+
+    /**
+     Sets the equals operator, copies the data from the rhs instance to this instance
+
+     @param const FrequencyDomainFeatures &rhs: another instance of the FrequencyDomainFeatures class from which the data will be copied to this instance
+     @return a reference to this instance of FrequencyDomainFeatures
+     */
+    FrequencyDomainFeatures& operator=(const FrequencyDomainFeatures &rhs);
+
+    /**
+     Sets the FeatureExtraction deepCopyFrom function, overwriting the base FeatureExtraction function.
+     This function is used to deep copy the values from the input pointer to this instance of the FeatureExtraction module.
+     This function is called by the GestureRecognitionPipeline when the user adds a new FeatureExtraction module to the pipeline.
+
+     @param FeatureExtraction *featureExtraction: a pointer to another instance of an FrequencyDomainFeatures, the values of that instance will be cloned to this instance
+     @return true if the deep copy was successful, false otherwise
+     */
+    virtual bool deepCopyFrom(const FeatureExtraction *featureExtraction);
+
+    /**
+     Sets the FeatureExtraction computeFeatures function, overwriting the base FeatureExtraction function.
+     This function is called by the GestureRecognitionPipeline when any new input data needs to be processed (during the prediction phase for example).
+     This function calls the FrequencyDomainFeatures's computeFrequencyDomainFeatures(...) function.
+
+     @param const VectorDouble &inputVector: the inputVector that should be processed.  Must have the same dimensionality as the FeatureExtraction module
+     @return true if the data was processed, false otherwise
+     */
+    virtual bool computeFeatures(const VectorDouble &inputVector);
+
+    /**
+     Sets the FeatureExtraction clear function, overwriting the base FeatureExtraction function.
+     This function is called by the GestureRecognitionPipeline when the pipelines main clear() function is called.
+     This function completely clears the FrequencyDomainFeatures setup, you will need to initialize the instance again before you can use it.
+
+     @return true if the FrequencyDomainFeatures was reset, false otherwise
+     */
+    virtual bool clear();
+
+    /**
+     Sets the FeatureExtraction reset function, overwriting the base FeatureExtraction function.
+     This function is called by the GestureRecognitionPipeline when the pipelines main reset() function is called.
+     This function resets the FrequencyDomainFeatures by re-initiliazing the instance.
+
+     @return true if the FrequencyDomainFeatures was reset, false otherwise
+     */
+    virtual bool reset();
+
+    /**
+     This saves the feature extraction settings to a file.
+     This overrides the saveSettingsToFile function in the FeatureExtraction base class.
+
+     @param fstream &file: a reference to the file to save the settings to
+     @return returns true if the settings were saved successfully, false otherwise
+     */
+    virtual bool saveModelToFile(fstream &file) const;
+
+    /**
+     This loads the feature extraction settings from a file.
+     This overrides the loadSettingsFromFile function in the FeatureExtraction base class.
+
+     @param fstream &file: a reference to the file to load the settings from
+     @return returns true if the settings were loaded successfully, false otherwise
+     */
+    virtual bool loadModelFromFile(fstream &file);
+
+    FFT fft;
+    FFTFeatures fftf;
+protected:
+    static RegisterFeatureExtractionModule< FrequencyDomainFeatures > registerModule;
+};
+
+} //End of namespace GRT
+
+#endif //GRT_FrequencyDomainFeatures_HEADER
